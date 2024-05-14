168ab6466083665e2976fc3a5730c6d53296e6ab
Fabian <fabian.leipold@informatik.hs-fulda.de>
2024-05-14 16:56:59 +0200

    write and read file, put - delete missing

--- routes/blog.js
+++ routes/blog.js
@@ -1,49 +1,47 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var safe =[];
+let safe = [];
 
-// Lesen der Datei posts.json beim Serverstart
-fs.readFile('posts.json', 'utf8', function(err, data) {
-    if (err) throw err;
-    var objs = data.split('\n');
-    objs.forEach(function(obj) {
-        if (obj) {
-            safe.push(JSON.parse(obj));
-            console.log('Loaded post: ' + obj);
-        }
-    });
+data = fs.readFileSync('posts.json');
+if (data.length > 0) {
+    safe = JSON.parse(data);
     console.log('Loaded posts from posts.json');
-});
+}
+else {
+    console.log('No posts found in posts.json');
+}
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('index', { title: 'Blog', name: 'Fabian' });
 });
-  
-router.post('/', function(req, res) {
-    let obj ={
-        blogID : req.body.blogID,
-        year : req.body.year,
-        month : req.body.month,
-        day : req.body.day,
-        author : req.body.author,
-        title : req.body.title,
-        text : req.body.text
-    }
- 
 
+
+router.post('/', function (req, res) {
+    let obj = {
+        blogID: req.body.blogID,
+        year: req.body.year,
+        month: req.body.month,
+        day: req.body.day,
+        author: req.body.author,
+        title: req.body.title,
+        text: req.body.text
+    }
+
+    console.log(safe);
     // Post-Befehle werden hier gespeichert
     safe.push(obj);
-    fs.appendFile('posts.json', JSON.stringify(obj) + '\n', function(err) {
+    console.log(safe);
+    fs.writeFileSync('posts.json', JSON.stringify(safe, null, 2), function (err) {
         if (err) throw err;
         console.log('Saved!');
     });
 
     //res.json({message: 'Blog post received', year: year, month: month, day: day, author: author, title: title, text: text});
-    res.json({message: 'Blog post received', obj});
+    res.json({ message: 'Blog post received', obj });
 });
 
-router.get('/:id', function(req, res) {
+router.get('/:id', function (req, res) {
     var id = req.params.id;
     var post = safe.find(post => post.blogID == id);
     if (post) {
@@ -53,13 +51,30 @@ router.get('/:id', function(req, res) {
     }
 });
 
-router.delete('/:page', function(req, res) {
-    let page = req.params.page;
-    res.send('Seite ' + page +' wurde gelöscht!');
-}); 
-router.put('/:page', function(req, res) {
-    let page = req.params.page;
-    res.send('Seite ' + page +' wurde geändert!');
+
+router.put('/:blogID', function (req, res) {
+    let blogID = req.params.blogID;
+    // Find the post with the given blogID
+    let postIndex = safe.findIndex(safe => safe.blogID == blogID);
+    console.log("POST"+postIndex);
+    if(postIndex > -1){
+        safe[postIndex].year = req.body.year;
+        safe[postIndex].month = req.body.month;
+        safe[postIndex].day = req.body.day;
+        safe[postIndex].author = req.body.author;
+        safe[postIndex].title = req.body.title;
+        safe[postIndex].text = req.body.text;
+    
+        fs.writeFileSync('posts.json', JSON.stringify(safe, null, 2), function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });
+
+        res.send('Post ' + blogID + ' has been updated!');
+    } else {
+        res.status(404).send('Post not found');
+    }
 });
+
 
 module.exports = router;
