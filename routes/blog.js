656baf9af451999dda7e547ab7e83699a2a8fe02
Fabian L <fabian.leipold@informatik.hs-fulda.de>
2024-05-14 14:37:35 +0200

    14.05.2024

--- routes/blog.js
+++ routes/blog.js
@@ -3,13 +3,26 @@ var router = express.Router();
 var fs = require('fs');
 var safe =[];
 
+// Lesen der Datei posts.json beim Serverstart
+fs.readFile('posts.json', 'utf8', function(err, data) {
+    if (err) throw err;
+    var objs = data.split('\n');
+    objs.forEach(function(obj) {
+        if (obj) {
+            safe.push(JSON.parse(obj));
+            console.log('Loaded post: ' + obj);
+        }
+    });
+    console.log('Loaded posts from posts.json');
+});
+
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Blog', name: 'Fabian' });
-  });
+});
   
-
 router.post('/', function(req, res) {
     let obj ={
+        blogID : req.body.blogID,
         year : req.body.year,
         month : req.body.month,
         day : req.body.day,
@@ -21,18 +34,25 @@ router.post('/', function(req, res) {
 
     // Post-Befehle werden hier gespeichert
     safe.push(obj);
-    fs.appendFile('posts.json', JSON.stringify(safe) + '\n', function(err) {
+    fs.appendFile('posts.json', JSON.stringify(obj) + '\n', function(err) {
         if (err) throw err;
         console.log('Saved!');
     });
 
-    res.json({message: 'Blog post received', year: year, month: month, day: day, author: author, title: title, text: text});
+    //res.json({message: 'Blog post received', year: year, month: month, day: day, author: author, title: title, text: text});
+    res.json({message: 'Blog post received', obj});
 });
 
-router.get('/:page', function(req, res) {
-    let page = req.params.page;
-    res.send('This is page ' + page);
+router.get('/:id', function(req, res) {
+    var id = req.params.id;
+    var post = safe.find(post => post.blogID == id);
+    if (post) {
+        res.json(post);
+    } else {
+        res.status(404).send('Post not found');
+    }
 });
+
 router.delete('/:page', function(req, res) {
     let page = req.params.page;
     res.send('Seite ' + page +' wurde gelöscht!');
